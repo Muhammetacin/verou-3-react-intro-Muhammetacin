bedcac3d5acf13517846a542ab92e10e899e7c08
Muhammet Cin <muhammetacin@gmail.com>
2022-04-12 13:32:25 +0200

    Add counter page with button

---
+++ src/Counter.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="bg-purple-200 flex flex-col justify-center text-center mx-auto h-screen">
+      <h2 className="text-4xl font-semibold">
+        You clicked <span className="text-violet-600 font-bold">{count}</span>{" "}
+        times
+      </h2>
+      <button
+        className="bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 text-white m-6 py-2.5 px-8 self-center rounded-full"
+        onClick={() => setCount(count + 1)}
+      >
+        Click me
+      </button>
+    </div>
+  );
+};
+
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import Counter from "./Counter";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="counter" element={<Counter />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
